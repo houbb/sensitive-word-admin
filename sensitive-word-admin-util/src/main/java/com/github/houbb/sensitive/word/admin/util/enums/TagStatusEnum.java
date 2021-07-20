cf9781f3b123adf21c50e227c3319b7d893d4cdc
binbin.hou <binbin.hou@huifu.com>
2021-07-20 16:38:12 +0800

    add for enums

---
+++ sensitive-word-admin-util/src/main/java/com/github/houbb/sensitive/word/admin/util/enums/TagStatusEnum.java
@@ -0,0 +1,37 @@
+package com.github.houbb.sensitive.word.admin.util.enums;
+
+/**
+* 枚举值
+* @author binbin.hou
+*/
+public enum TagStatusEnum {
+    /**
+    * 枚举值
+    */
+    F("F", "失效"),
+    S("S", "正常"),
+    ;
+
+    /**
+    * 编码
+    */
+    private final String code;
+
+    /**
+    * 描述
+    */
+    private final String desc;
+
+    TagStatusEnum(String code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
---
+++ sensitive-word-admin-util/src/main/java/com/github/houbb/sensitive/word/admin/util/enums/WordTypeEnum.java
@@ -0,0 +1,37 @@
+package com.github.houbb.sensitive.word.admin.util.enums;
+
+/**
+* 枚举值
+* @author binbin.hou
+*/
+public enum WordTypeEnum {
+    /**
+    * 枚举值
+    */
+    ALLOW("ALLOW", "允许"),
+    DENY("DENY", "禁止"),
+    ;
+
+    /**
+    * 编码
+    */
+    private final String code;
+
+    /**
+    * 描述
+    */
+    private final String desc;
+
+    WordTypeEnum(String code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
